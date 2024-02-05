39951e6587a46ae0ce706979b318aea1b38ccf51
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-02-05 11:27:43 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -8,10 +8,10 @@ import {
   Percentage,
 } from './Statistics.styled';
 
-const Statistics = ({ stats }) => {
+const Statistics = ({ stats, title }) => {
   return (
     <StatisticsSection>
-      <Title>Upload stats</Title>
+      {title && <Title>{title}</Title>}
       <StatList>
         {stats.map(stat => (
           <StatItem
